f576cfac0c3c97bc9340fef953e22fe5444a9463
unknown <Ur GitHub email>
2021-08-16 17:50:40 +0700

    first commit 2

---
+++ ChitChat/src/main/java/com/example/service/MessageService.java
@@ -0,0 +1,17 @@
+package com.example.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.model.Message;
+
+
+public interface MessageService {
+	List<Message> findAll();
+	Optional<Message> findLatestMessage(String username);
+    void save(Message message);
+    void remove(Message message);
+    Optional<Message> findById(Long id);
+    List<Message> findByUsername(String username);
+
+}
