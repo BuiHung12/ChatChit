16cbb0d914e02961af00a76030401310eb80c278
unknown <Ur GitHub email>
2021-08-18 10:38:17 +0700

    commit 17

--- src/main/java/com/example/ChitChat/ChitChatApplication.java
+++ src/main/java/com/example/ChitChat/ChitChatApplication.java
@@ -2,8 +2,10 @@ package com.example.ChitChat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value = "com.example.ChitChat.repository")
 public class ChitChatApplication {
 
 	public static void main(String[] args) {
