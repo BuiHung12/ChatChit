1bb577e23f84822584931f9cd235b44e4150c655
unknown <Ur GitHub email>
2021-08-18 12:07:43 +0700

    commit 22

--- src/main/java/com/example/ChitChat/ChitChatApplication.java
+++ src/main/java/com/example/ChitChat/ChitChatApplication.java
@@ -2,11 +2,12 @@ package com.example.chitchat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(value = "com.example.chitchat.repository")
-public class ChitChatApplication {
+public class ChitChatApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChitChatApplication.class, args);
